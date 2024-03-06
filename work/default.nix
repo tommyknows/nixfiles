5f2741f721766836723d0fccfeffe46f1df344c5
Ramon Rüttimann <me@ramonr.ch>
2024-03-06 11:44:38 +0100

    configure fnm in work module

--- work/default.nix
+++ work/default.nix
@@ -14,6 +14,8 @@
   set -gx WORK_GITHUB_USER "snyk"
 
   # Required for fnm (npm version manager...)
+  # For some reason, this adds the fnm_multishells path to the PATH variable TWICE:
+  # once as one of the last entries in PATH, and one where I would expect it given the load order...
   fnm env --use-on-cd | source
 
   set -gx SNYK_API_TOKEN (security find-generic-password -a "ramon.ruttimann@snyk.io" -s "Snyk API Token" -w)
